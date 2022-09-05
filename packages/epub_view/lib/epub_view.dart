d150df1b56839d653db16940cfcd02a844552afd
Aleksandr Bolotov <Alexander.Bolotov@onlyoffice.com>
2022-09-05 17:19:06 +0300

    scroll to current chapter in menu

--- packages/epub_view/lib/epub_view.dart
+++ packages/epub_view/lib/epub_view.dart
@@ -1,9 +1,11 @@
 export 'package:epubx/epubx.dart';
 
+export 'src/data/epub_cfi_reader.dart';
+export 'src/data/epub_parser.dart';
 export 'src/data/models/chapter.dart';
+export 'src/data/models/chapter_view_value.dart';
 export 'src/data/models/chapter_view_value.dart';
 export 'src/helpers/epub_document.dart';
 export 'src/ui/actual_chapter.dart';
 export 'src/ui/epub_view.dart';
 export 'src/ui/table_of_contents.dart';
-export 'src/data/models/chapter_view_value.dart';
--- packages/epub_view/lib/src/ui/table_of_contents.dart
+++ packages/epub_view/lib/src/ui/table_of_contents.dart
@@ -8,11 +8,13 @@ class EpubViewTableOfContents extends StatelessWidget {
     this.padding,
     this.itemBuilder,
     this.loader,
+    this.currentChapterKey,
     Key? key,
   }) : super(key: key);
 
   final EdgeInsetsGeometry? padding;
   final EpubController controller;
+  final GlobalKey? currentChapterKey;
 
   final Widget Function(
     BuildContext context,
@@ -22,39 +24,50 @@ class EpubViewTableOfContents extends StatelessWidget {
   )? itemBuilder;
   final Widget? loader;
 
+  void _scrollByContext(BuildContext? context) {
+    if (context != null) {
+      Scrollable.ensureVisible(context,
+          alignment: 0.5, duration: const Duration(milliseconds: 0));
+    }
+  }
+
   @override
-  Widget build(BuildContext context) =>
-      ValueListenableBuilder<List<EpubViewChapter>>(
-        valueListenable: controller.tableOfContentsListenable,
-        builder: (_, data, child) {
-          Widget content;
+  Widget build(BuildContext context) {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollByContext(currentChapterKey?.currentContext);
+    });
+    return ValueListenableBuilder<List<EpubViewChapter>>(
+      valueListenable: controller.tableOfContentsListenable,
+      builder: (_, data, child) {
+        Widget content;
 
-          if (data.isNotEmpty) {
-            content = ListView.builder(
-              padding: padding,
-              key: Key('$runtimeType.content'),
-              itemBuilder: (context, index) =>
-                  itemBuilder?.call(context, index, data[index], data.length) ??
-                  ListTile(
-                    title: Text(data[index].title!.trim()),
-                    onTap: () =>
-                        controller.scrollTo(index: data[index].startIndex),
-                  ),
-              itemCount: data.length,
-            );
-          } else {
-            content = KeyedSubtree(
-              key: Key('$runtimeType.loader'),
-              child: loader ?? const Center(child: CircularProgressIndicator()),
-            );
-          }
-
-          return AnimatedSwitcher(
-            duration: const Duration(milliseconds: 250),
-            transitionBuilder: (Widget child, Animation<double> animation) =>
-                FadeTransition(opacity: animation, child: child),
-            child: content,
+        if (data.isNotEmpty) {
+          content = ListView.builder(
+            padding: padding,
+            key: Key('$runtimeType.content'),
+            itemBuilder: (context, index) =>
+                itemBuilder?.call(context, index, data[index], data.length) ??
+                ListTile(
+                  title: Text(data[index].title!.trim()),
+                  onTap: () =>
+                      controller.scrollTo(index: data[index].startIndex),
+                ),
+            itemCount: data.length,
           );
-        },
-      );
+        } else {
+          content = KeyedSubtree(
+            key: Key('$runtimeType.loader'),
+            child: loader ?? const Center(child: CircularProgressIndicator()),
+          );
+        }
+
+        return AnimatedSwitcher(
+          duration: const Duration(milliseconds: 250),
+          transitionBuilder: (Widget child, Animation<double> animation) =>
+              FadeTransition(opacity: animation, child: child),
+          child: content,
+        );
+      },
+    );
+  }
 }
